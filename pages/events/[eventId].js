5fdf7dad75968a369dcc09ca2e6ea4faef94e9f7
Marchinka90 <martin_marchev@abv.bg>
2023-11-18 19:11:37 +0200

    Added page pre-rendering and data fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,30 +1,26 @@
-import { useRouter } from 'next/router';
 import { Fragment } from 'react';
 
-import { getEventById } from '../../dummy-data';
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 import EventSummery from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 import Button from '../../components/ui/button';
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
       <Fragment>
-        <ErrorAlert>
-          <p>No event found!</p>
-        </ErrorAlert>
-        <div className='center'>
-          <Button link='/events'>Show All Events</Button>
+        <div className='center' >
+          <p>Loading...</p>
         </div>
+        {/* <div className='center'>
+          <Button link='/events'>Show All Events</Button>
+        </div> */}
       </Fragment>
-      );
+    );
   }
 
   return (
@@ -41,6 +37,28 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const selectedEvent = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: selectedEvent
+    },
+    revalidate: 30 // regenarate every 30 sec
+  }
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+  const paths = events.map(event => ({params: {eventId: event.id}}))
+
+  return {
+    paths: paths,
+    fallback: 'blocking' //genereating the entire page first, then send it to the user
+  }
 }
 
 export default EventDetailPage
