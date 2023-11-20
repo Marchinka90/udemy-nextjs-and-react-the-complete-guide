0c8d5ef393ad4a288428780946592ba9d9752151
Marchinka90 <martin_marchev@abv.bg>
2023-11-21 00:33:06 +0200

    Added context state and notifications for fetching data

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,12 +1,52 @@
+import { useRef, useContext } from 'react';
+import NotificationContext from '../../store/notification-context';
+
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+  const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
+
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter.',
+      status: 'pending'
+    });
+
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+        return response.json().then(data => {
+          throw new Error(data.message || 'Something went wrong!');
+        })
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success',
+          message: 'Successfuly registered for newsletter!',
+          status: 'success'
+        });
+      })
+      .catch(error => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'error'
+        });
+      });
   }
 
   return (
@@ -19,6 +59,7 @@ function NewsletterRegistration() {
             id='email'
             placeholder='Your email'
             aria-label='Your email'
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
